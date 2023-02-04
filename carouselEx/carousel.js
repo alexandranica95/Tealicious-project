bbbfbe84c55af473e6207bd28372ffdeb06a6c87
alexandranica95 <alexandranica64@gmail.com>
2023-02-04 14:11:05 +0200

    added category page

--- carouselEx/carousel.js
+++ carouselEx/carousel.js
@@ -1,6 +1,6 @@
 
 const buttonsWrapper = document.querySelector(".map");
-const slides = document.querySelector(".inner");
+const slides = document.querySelector(".bubble-tea-products");
 
 buttonsWrapper.addEventListener("click", e => {
   if (e.target.nodeName === "BUTTON") {
@@ -19,3 +19,31 @@ buttonsWrapper.addEventListener("click", e => {
     }
   }
 });
+
+const productsWrapper = document.querySelector(".bar-container")
+const bubbleTeaProducts = Array.from(document.querySelector(".wrapper").children);
+const mapElement = document.querySelector(".map");
+
+productsWrapper.addEventListener("click", e => {
+  Array.from(productsWrapper.children).forEach(item => item.classList.remove("focus"));
+  e.target.classList.add("focus");
+  if (e.target.classList.contains("first")) {
+    bubbleTeaProducts[0].classList.remove("display-none")
+    mapElement.classList.remove("display-none");
+
+    bubbleTeaProducts[1].classList.add("display-none")
+    bubbleTeaProducts[2].classList.add("display-none")
+  } else if (e.target.classList.contains("second")) {
+    bubbleTeaProducts[1].classList.remove("display-none")
+
+    mapElement.classList.add("display-none");
+    bubbleTeaProducts[0].classList.add("display-none")
+    bubbleTeaProducts[2].classList.add("display-none")
+  } else if (e.target.classList.contains('third')){
+    bubbleTeaProducts[2].classList.remove("display-none")
+
+    mapElement.classList.add("display-none");
+    bubbleTeaProducts[0].classList.add("display-none")
+    bubbleTeaProducts[1].classList.add("display-none")
+  }
+})
